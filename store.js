09abb2a110638f58af11f22149d33047fcde6952
baifeifie <38942692@qq.com>
2017-03-24 00:17:15 +0800

    取消redis密码

--- store.js
+++ store.js
@@ -6,7 +6,7 @@ class Store {
             port: 6379,          // Redis port
             host: '127.0.0.1',   // Redis host
             family: 4,           // 4 (IPv4) or 6 (IPv6)
-            password: 'zwr0925',
+            password: '',
             db: 0
         })
     }
